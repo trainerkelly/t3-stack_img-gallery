7b7a4a7cb191fe3b778792986609740a752fe015
Kelly Mulry <trainerkelly@live.com>
2024-12-28 10:12:44 -0500

    add auth with clerk

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,18 @@
+import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
